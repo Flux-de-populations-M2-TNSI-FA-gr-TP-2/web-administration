d198021b6b6f45b5607cce0490eb208bb4fbd67b
bossmaleo <bossprogrammaleo@gmail.com>
2019-12-12 11:21:01 +0100

    version sans bug

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -10,8 +10,7 @@ import {Router} from '@angular/router';
 })
 export class ConnexionComponent implements OnInit {
 
-  constructor(private  router: Router
-    , private httpClient: HttpClient) { }
+  constructor(private  router: Router) { }
 
   ngOnInit() {
   }
