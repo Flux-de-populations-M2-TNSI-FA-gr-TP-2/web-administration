856f3e39aafd3ed4f888f3d540777772252f1e24
Regis Maleo <bossprogrammaleo@gmail.com>
2019-12-17 10:36:03 +0100

    Affichage des établissements avec leurs salles

--- src/app/etablissements/etablissements.component.ts
+++ src/app/etablissements/etablissements.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ConstService} from '../service/Const.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AuthService} from '../service/auth.service';
 
 @Component({
   selector: 'app-etablissements',
@@ -8,9 +10,43 @@ import {ConstService} from '../service/Const.service';
 })
 export class EtablissementsComponent implements OnInit {
 
-  constructor(private constance: ConstService) { }
+  ourobject: any;
+  batimentlist: any;
+
+  constructor(private constance: ConstService
+    , private httpClient: HttpClient
+    , private authService: AuthService) { }
 
   ngOnInit() {
+    this.getListebatiment();
+  }
+
+
+  getListebatiment() {
+
+    const url = 'https://fluxtnsi.ddns.net/api/location';
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + this.authService.sessions.access_token
+      })
+    };
+
+    this.httpClient
+      .get(url, httpOptions)
+      .subscribe(
+        (response) => {
+          this.ourobject = response;
+          this.batimentlist = this.ourobject.data;
+          //console.log(this.utilisateurslists);
+          console.log(response);
+          return response;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 
 }
